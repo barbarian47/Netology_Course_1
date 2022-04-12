c89d6213ef3b6c33601e27ec6f08d69ea28ed80d
Alexey Serebryakov <barbarians47@outlook.com>
2022-04-13 02:13:56 +0300

    Init commit

---
+++ token_47.py
@@ -0,0 +1,12 @@
+vk_token = '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008'
+vk_id_client = 552934290
+
+
+ok_application_id = ''
+ok_access_token = ''
+ok_session_secret_key = ''
+ok_application_key = ''
+ok_secret_key = ''
+
+
+y_token = ''
